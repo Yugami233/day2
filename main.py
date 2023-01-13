de9c4755af0c57273aa9e0813aecdd40ce51597c
Yugami233 <120727508+Yugami233@users.noreply.github.com>
2023-01-13 14:07:34 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+import json
+
+import pprint
+
+API = "https://api.safone.me/tmdb"
+
+NAME = input("enter name")
+
+params = {"query" : NAME}
+
+response = requests.get(API,params)
+
+data = json.loads(response.text)
+
+ title = data['name']
+
+ rating = data['rating']
+
+ poster link = data['poster']
+
+  
+
+ print(title)
+
+ print(data)
+
+ print(poster)
+
+  
+
+  
